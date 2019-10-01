0bcee0468f7cb9a06c78802ac9cb9b5bf0952b8a
SalemPCF <salem.cresswell@btinternet.com>
2019-10-01 09:45:14 +0100

    Added helpers

---
+++ js/helpers.js
@@ -0,0 +1,89 @@
+export const chunk = (array, length) => new Array(Math.ceil(array.length / length)).fill(0)
+    .map((i, n) => array.slice(n * length, n * length + length));
+
+export const ucFirst = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+
+/**
+ * Deeply clones an object.
+ *
+ * @param {Object} object - The object to clone.
+ *
+ * @return {Object}
+ */
+const cloneDeep = (object) => {
+    if (!object) {
+        return object;
+    }
+
+    const newObject = Array.isArray(object) ? [] : {};
+
+    Object.keys(object).forEach((key) => {
+        const c = object[key];
+
+        newObject[key] = (c === null ? null : (typeof c === 'object') ? cloneDeep(c) : c);
+    });
+
+    return newObject;
+};
+
+/**
+ * Edits properties of an object.
+ *
+ * @note To dive into multi-dimentional objects, supply a key with dot syntax.
+ *
+ * @param {Object} obj - The object to edit
+ * @param {Object} mixins - [{ key, value }]
+ *
+ * @return {Object}
+ *
+ * Usage:
+ *     const mixed = mixinParams(
+ *         { foo: 'bar', bar: { baz: 'foo' } },
+ *         [{ key: 'foo', value: 'foo' }, { key: 'bar.baz', value: 'baz' }]
+ *     )
+ */
+export const editObject = (obj, mixins) => {
+    const mixed = cloneDeep(obj);
+
+    mixins.forEach(({ key, value }) => {
+        // We're attempting to access an object
+        if (key.indexOf('.') > -1) {
+            const keys = key.split('.');
+
+            keys.reduce((acc, k, index) => (index === keys.length - 1
+                // We're at the top of our tree!
+                ? (acc[k] = value)
+                // We've still got a tree to climb!
+                : acc[k] || (acc[k] = {})
+            ), mixed);
+        } else {
+            mixed[key] = value;
+        }
+    });
+
+    return mixed;
+};
+
+/**
+ * Throttles a function call.
+ *
+ * @param {Function} func - The function
+ * @param {Number} limit - The time before the function can be called again
+ *
+ * Usage: throttle(() => console.log('hi'), 1000)
+ */
+export const throttle = (func, limit = 250) => {
+    let inThrottle;
+
+    return (...args) => {
+        const context = this;
+
+        if (!inThrottle) {
+            func.apply(context, args);
+
+            inThrottle = true;
+
+            setTimeout(() => (inThrottle = false), limit);
+        }
+    };
+};
--- maths/randomisers.js
+++ maths/randomisers.js
@@ -1,12 +1,39 @@
-export const generateRandomInt = (min, max) => Math.floor((Math.random() * ((max - min) + 1)) + min);
+export const randomInt = (min, max) => Math.floor((Math.random() * ((max - min) + 1)) + min);
 
-export const generateRandomString = (length = 20) => {
+export const randomString = (length = 20) => {
     let text = '';
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
     for (let i = 0; i < length; i += 1) {
-        text += possible.charAt(generateRandomInt(0, possible.length - 1));
+        text += possible.charAt(randomInt(0, possible.length - 1));
     }
 
     return text;
+};
+
+export const randomColor = () => `#${[0, 0, 0].map(() => (`0${randomInt(10, 255).toString(16)}`).slice(-2)).join('')}`;
+
+/**
+ * Generates a random HSL value.
+ *
+ * @param {Number} initialH - Hue
+ * @param {Number} s - Saturation
+ * @param {Number} l - Lightness
+ * @param {Number} adjuster - Will slightly adjust the hue, can be used to prevent certain colours.
+ *
+ * @return {String}
+ */
+export const randomHSL = (initialH = randomInt(0, 360), s = randomInt(55, 85), l = randomInt(30, 70), adjuster = 0) => {
+    const adjuster = 8;
+
+    const h = randomInt(
+        initialH - adjuster < 0
+            ? 0
+            : initialH - adjuster,
+        initialH + adjuster > 360
+            ? 360
+            : initialH + adjuster,
+    );
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
 };
